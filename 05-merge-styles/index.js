9ff0e81c27a2d409a893b418233862f31960de88
Clukva <berries.whale@gmail.com>
2024-01-22 00:11:26 +0300

    refactor: replace firbidden sync function

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -6,37 +6,38 @@ const outputDir = path.join(__dirname, 'project-dist');
 const outputFile = path.join(outputDir, 'bundle.css');
 
 function createCssBundle() {
-  fs.mkdirSync(outputDir, { recursive: true });
-
-  let stylesBundle = [];
-
-  fs.readdir(stylesDir, { withFileTypes: true }, (err, files) => {
+  fs.mkdir(outputDir, { recursive: true }, (err) => {
     if (err) throw err;
+    let stylesBundle = [];
 
-    let cssFiles = files.filter(
-      (file) => file.isFile() && path.extname(file.name) === '.css',
-    );
+    fs.readdir(stylesDir, { withFileTypes: true }, (err, files) => {
+      if (err) throw err;
 
-    let fileReads = cssFiles.map((file) => {
-      return new Promise((resolve, reject) => {
-        fs.readFile(path.join(stylesDir, file.name), 'utf-8', (err, data) => {
-          if (err) reject(err);
-          else resolve(data);
+      let cssFiles = files.filter(
+        (file) => file.isFile() && path.extname(file.name) === '.css',
+      );
+
+      let fileReads = cssFiles.map((file) => {
+        return new Promise((resolve, reject) => {
+          fs.readFile(path.join(stylesDir, file.name), 'utf-8', (err, data) => {
+            if (err) reject(err);
+            else resolve(data);
+          });
         });
       });
+
+      Promise.all(fileReads)
+        .then((values) => {
+          stylesBundle = values.join('\n');
+          fs.writeFile(outputFile, stylesBundle, (err) => {
+            if (err) throw err;
+            console.log('bundle.css has been created!');
+          });
+        })
+        .catch((err) => {
+          console.error('Error reading CSS files:', err);
+        });
     });
-
-    Promise.all(fileReads)
-      .then((values) => {
-        stylesBundle = values.join('\n');
-        fs.writeFile(outputFile, stylesBundle, (err) => {
-          if (err) throw err;
-          console.log('bundle.css has been created!');
-        });
-      })
-      .catch((err) => {
-        console.error('Error reading CSS files:', err);
-      });
   });
 }
 
